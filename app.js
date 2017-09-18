977714bbe42165f2da75282445063150836287bc
India Amos <india@indiamos.com>
2017-09-18 16:41:42 -0400

    Add manual logging

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
+app.use((req, res, next) => {
+  console.log(req.method, req.url, res.statusCode);
+  next();
+})
+
+app.get('/', (req, res, next) => {
   res.send('Hello, world!');
+})
+
+app.use('/news', (req, res, next) => {
+  console.log('This is /news');
+  next();
+})
+
+app.get('/news', (req, res, next) => {
+  res.send('Welcome to /news!');
 })
 
 app.listen(8181, () => {
